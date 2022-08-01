d94398948799318f6d5ca5ca939ec7036171991d
Nikhilkum9 <75990125+Nikhilkum9@users.noreply.github.com>
2022-08-01 13:11:00 +0530

    Add files via upload

---
+++ Checked_exeption/Checked_exeption/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Checked_exeption
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            checked
+            {
+                int val = int.MaxValue;
+                Console.WriteLine(val + 2);
+            }
+        }
+    }
+}
